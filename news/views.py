b39c01f13bad1b9455868e09ebda4d680c52d754
iceiceiceice <335998869@qq.com>
2017-06-29 04:04:15 +0800

    推荐算法

--- news/views.py
+++ news/views.py
@@ -7,6 +7,8 @@ from newsbackend.common import respond_assemble
 from users.models import UserSetting
 import getnews
 
+c_list = ['finance','sports','ent','mil','edu','tech','nba','stock']
+
 def showindex(request):
     return render(request, 'index.html')
 
@@ -21,7 +23,7 @@ def change2json(news_list):
         news_json['weburl'] = news.weburl
         news_json['time'] = news.time
         news_json['pic'] = news.pic
-        #print(news_json)
+        print(news_json)
         news_json_list.append(news_json)
     return  news_json_list
 
@@ -51,6 +53,45 @@ def update_setting(channel, likelist, num):
         likelist.stock = likelist.stock+num
         likelist.save()
 
+def get_rec(likelist,num,base):
+    ch_list = dict(finance=0,sports=0,ent=0,mil=0,edu=0,tech=0,nba=0,stock=0,sum=0)
+    sum = 0.0
+    ch_list['finance'] = likelist.finance
+    sum += likelist.finance
+    ch_list['sports'] = likelist.sports
+    sum += likelist.sports
+    ch_list['ent'] = likelist.ent
+    sum += likelist.ent
+    ch_list['mil'] = likelist.mil
+    sum += likelist.mil
+    ch_list['edu'] = likelist.edu
+    sum += likelist.edu
+    ch_list['tech'] = likelist.tech
+    sum += likelist.tech
+    ch_list['nba'] = likelist.nba
+    sum += likelist.nba
+    ch_list['stock'] = likelist.stock
+    sum += likelist.stock
+    news_list = []
+    allnewslist = Mynews.objects.order_by('-id')
+    for ch in c_list:
+        print(ch_list[ch])
+        ch_list[ch] /=sum
+        ch_list[ch] *= 10
+        print(ch_list[ch])
+        avg = int(round(ch_list[ch]))
+        print(avg)
+        news = allnewslist.filter(channel=ch)
+        if avg > 1:
+            for i in range(avg):
+                news_list.append(news.all()[base*avg+i])
+        elif avg == 1:
+            print(news.all()[i].title)
+            news_list.append(news.all()[base])
+
+        #print(news_list)
+    return news_list
+
 @csrf_exempt
 def get_news(request):
     channel = request.POST.get('channel')
@@ -64,8 +105,11 @@ def get_news(request):
 
     if channel == 'rec':
         if request.user.is_authenticated():
-            last = Mynews.objects.last()
-            news_list = Mynews.objects.filter(id__gt = last.id-10)
+            likelist = UserSetting.objects.get(userid=user.id)
+            news_list = get_rec(likelist,num,begin/10)
+
+            # last = Mynews.objects.last()
+            # news_list = Mynews.objects.filter(id__gt = last.id-10)
         else:
             news_channel = Mynews.objects.filter(channel='hot')
             order_news = news_channel.order_by('-id')
@@ -110,8 +154,14 @@ def news_update(request):
 
 @csrf_exempt
 def news_content(request):
-    #newsid = int(request.GET.get('newsid'))
-    #print(newsid)
+    newsid = int(request.GET.get('newsid'))
+    newscontent = Mynews.objects.get(id = newsid)
+    channel = newscontent.channel
+    if request.user.is_authenticated():
+        user = request.user
+        likelist = UserSetting.objects.get(userid=user.id)
+        update_setting(channel,likelist,5)
+
     return render(request,'newscontent.html')
 
 @csrf_exempt
